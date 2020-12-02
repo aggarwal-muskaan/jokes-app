7547e55c9bc9601fe6204a2876633078e30d7c9b
Muskaan Aggarwal <muskaan19010@gmail.com>
2020-12-02 21:11:45 +0530

    add dynamic color & emojis

--- src/Joke.js
+++ src/Joke.js
@@ -2,7 +2,24 @@ import React, { Component } from "react";
 import "./Joke.css";
 
 class Joke extends Component {
+  getReaction() {
+    if (this.props.votes >= 15)
+      return { color: "#4CAF50", emo: "em em-rolling_on_the_floor_laughing" };
+    else if (this.props.votes >= 12)
+      return { color: "#8BC34A", emo: "em em-laughing" };
+    else if (this.props.votes >= 9)
+      return { color: "#CDDC39", emo: "em em-smiley" };
+    else if (this.props.votes >= 6)
+      return { color: "#FFEB3B", emo: "em em-slightly_smiling_face" };
+    else if (this.props.votes >= 3)
+      return { color: "#FFC107", emo: "em em-neutral_face" };
+    else if (this.props.votes >= 0)
+      return { color: "#FF9800", emo: "em em-confused" };
+    else return { color: "#f444336", emo: "em em-angry" };
+  }
+
   render() {
+    const obj = this.getReaction();
     return (
       <div className="Joke ">
         {/* <li>{this.props.text}</li> */}
@@ -14,7 +31,9 @@ class Joke extends Component {
           >
             <path d="M10 0a10 10 0 1 1 0 20 10 10 0 0 1 0-20zm0 2a8 8 0 1 0 0 16 8 8 0 0 0 0-16zm2 8v5H8v-5H5l5-5 5 5h-3z" />
           </svg>
-          <span className="Joke-votes">{this.props.votes}</span>
+          <span className="Joke-votes" style={{ color: obj.color }}>
+            {this.props.votes}
+          </span>
           <svg
             onClick={this.props.downvote}
             xmlns="http://www.w3.org/2000/svg"
@@ -27,10 +46,7 @@ class Joke extends Component {
         <div className="Joke-text">{this.props.text}</div>
 
         <div className="Joke-emo">
-          <i
-            class="em em-rolling_on_the_floor_laughing"
-            aria-label="ROLLING ON THE FLOOR LAUGHING"
-          ></i>
+          <i class={obj.emo} aria-label="ROLLING ON THE FLOOR LAUGHING"></i>
         </div>
       </div>
     );
